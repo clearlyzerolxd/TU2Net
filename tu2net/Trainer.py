f49d3d750550459b887868aacef8ce2cdea4070e
clearlyzerolxd <clearlyzero@stu.cqut.edu.cn>
2023-12-07 10:56:55 +0800

    Training

--- tu2net/Trainer.py
+++ tu2net/Trainer.py
@@ -5,9 +5,28 @@ import torch.optim as optim
 from torch.utils import tensorboard
 from TU2Net import Generator_full
 from Discriminator import Spatial,Temporal
+from torch.utils.data import Dataset
+from torch.utils import data
+import os
+import numpy
+class MyDataset(Dataset):
+    """rainfall data """
+    def __init__(self,data_path):
+        super(MyDataset, self).__init__()
+        self.path =os.listdir(data_path)
+        self.root_path = data_path
+        print("There are {} sets of data in total".format(len(self.path)))
+    def __getitem__(self, index):
+        x = numpy.load(os.path.join(self.root_path,self.path[index]))
+        # print(x.shape)
+        
+        x = torch.from_numpy(x)
+        x = torch.clip(x,0,20)
+        x = x/20.0
+        return x[:4],x[4:10]
 
-
-
+    def __len__(self):
+        return len(self.path)
 
 
 
@@ -36,14 +55,23 @@ def train():
     Temporal_dis_optim = optim.Adam(Temporal_dis.parameters(),lr=2e-5,betas=(0.0,0.999))
     
     
-    
-    
     print("Spatial_dis_sum total number of parameters is {}MB \nTemporal_dis_sum total number of parameters is {}MB".format(round(Temporal_dis_sum,2),round(Spatial_dis_sum,2)))
     
     
     
     
+    print("ready dataset")
     
+    mydataset = MyDataset("tu2net/example")
+    
+    dataloader =data.DataLoader(mydataset, batch_size=2, shuffle=False)
+    
+    # x,y = next(iter(dataloader))
+    for i in range(500):
+        for step,data in dataloader:
+            
+            
+            
     
     
     
